7396a9496b9c71c425e34b33492f650dd16ed4d2
Dieggo <diego.fuentes@alu.uclm.es>
2023-03-24 12:32:38 +0100

    modificaciones a todo

--- src/PaqViviendas/CasadeCampo.java
+++ src/PaqViviendas/CasadeCampo.java
@@ -4,12 +4,14 @@ public class CasadeCampo extends Vivienda{
     private double metrosparcela;
     private Chalet chalet;
 
-    public CasadeCampo(double precio, double numerodeplantas) {
+    public CasadeCampo(double precio, double numerodeplantas, double metrosparcela, Chalet chalet) {
         super(precio, numerodeplantas);
+        this.metrosparcela = metrosparcela;
+        this.chalet = chalet;
     }
 
     @Override
     public double impuesto() {
-        return super.impuesto()+  0.5 * this.metrosparcela;
+        return super.impuesto()+ 0.5 * this.metrosparcela;
     }
 }
